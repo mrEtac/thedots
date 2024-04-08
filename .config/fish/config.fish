6a6e8dfa976d9772e1ac1b843c96448495cb0075
catErm <165536096+mrEtac@users.noreply.github.com>
2024-04-08 04:09:01 -0400

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+eval "$(oh-my-posh init fish --config /home/caterm/.cache/oh-my-posh/themes/velvet.omp.json)"
+    # Commands to run in interactive sessions can go here
+end
