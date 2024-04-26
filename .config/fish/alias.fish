19491fae6634a172e18d2acc8bee50eace7a6347
catErm <165536096+mrEtac@users.noreply.github.com>
2024-04-26 09:20:40 -0400

    Add files via upload

---
+++ .config/fish/alias.fish
@@ -0,0 +1,7 @@
+#fish alias
+
+alias neo="neofetch | lolcat"
+alias matrix="cmatrix | lolcat"
+alias ls="ls -a | lolcat"
+alias update="sudo pacman -Syu"
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,8 @@ if status is-interactive
 eval "$(oh-my-posh init fish --config /home/caterm/.cache/oh-my-posh/themes/velvet.omp.json)"
     # Commands to run in interactive sessions can go here
 end
+
+# Aliases
+if [ -f $HOME/.config/fish/alias.fish ]
+source $HOME/.config/fish/alias.fish
+end
